22d7c39f771c6a5a9dbf7e6d8a687a48ec0d5530
Artanisax <1415197174@qq.com>
2023-12-28 17:20:47 +0800

    try kubric

--- train.sh
+++ train.sh
@@ -3,18 +3,19 @@ python train.py \
 --num_workers 16 \
 --num_steps 6400 \
 --ckpt_path ./checkpoints \
---dataset_root /hhd3/GroupProject2023Fall/datasets \
+--dataset_root /nvme-ssd/lzl/datasets \
 --model_name cotracker \
 --sequence_len 24 \
 --traj_per_sample 384 \
---crop_size 512 640 \
 --sliding_window_len 8 \
 --updateformer_space_depth 6 \
 --updateformer_time_depth 6 \
---eval_datasets tapvid_davis_first badja \
---save_freq 16 \
+--save_freq 4 \
 --save_every_n_epoch 4 \
 --evaluate_every_n_epoch 4 \
---model_stride 4 \
---gpus 1 2 3
+--model_stride 16 \
+--gpus 0
+# --eval_datasets tapvid_davis_first badja \
+# --dataset_root /hhd3/GroupProject2023Fall/datasets \
+# --crop_size 512 640 \
 # --sample_vis_1st_frame \
